1c8cc4859899f0eaffab5094494d71b988f41e0e
arunpravin125@gmail.com <arunpravin125@gmail.com>
2024-09-17 13:31:41 +0530

    updated package.json file and backend file for render

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import path from "path"
 import dotenv from "dotenv"
 import { connectionDB } from "./DB/database.js"
 import { authRoutes } from "./Routes/auth.Route.js"
@@ -7,16 +8,28 @@ import { messageRouter } from "./Routes/message.routes.js"
 import cookieParser from "cookie-parser"
 import { userRouter } from "./Routes/users.routes.js"
 import {app, server } from "./socket/socket.js"
+
 app.use(express.json())
 app.use(cors())
 dotenv.config()
 app.use(cookieParser())
 
+const __dirname = path.resolve()
+
+const usePort = process.env.PORT || 4801
+
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+
+
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRouter)
 app.use("/api/users",userRouter)
 
-const usePort = process.env.PORT || 4801
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(usePort,()=>{
     connectionDB()
